687fffad988af9c9c355b1637f4069858739fd29
shibadog1121 <takahisa.sakurai@pc-12-361.local>
2019-04-16 22:59:09 +0900

    lesson2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,9 +10,12 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val text :String = "テキストだよ"
+
+    //var→codeで変更するとき val→変更しない(ObservableFieldだと変えられる)
+    var numberText :String = "0"
     val text1 :ObservableField<String> = ObservableField( "")
     val text2 :ObservableField<String> = ObservableField( "")
+    val symbol :ObservableField<SymbolTypeEnum> = ObservableField()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +34,73 @@ class MainActivity : AppCompatActivity() {
 
     fun selectButton(number: Int){
         text1.set(text2.get())
-        val result = number + Integer.parseInt(text2.get())
-        text2.set("" + result)
+        if (text2.get()=="0"){
+            text2.set("")
+        }
+        text2.set(text2.get() + number)
+    }
+
+    fun plusButton(){
+        numberText = text2.get().toString()
+        symbol.set(SymbolTypeEnum.PLUS)
+        text2.set("0")
+    }
+
+    fun equalButton(){
+        if(text2.get() == null){
+            return
+        }
+
+        if(symbol.get() == null){
+            return
+        }
+
+//        こうともかける
+//        text2.get()?.let {
+//            symbol.get()?.let {
+//
+//            }
+//
+//        }
+
+
+       // kotlinだとswitchではなくwhen
+        when(symbol.get()){
+            SymbolTypeEnum.PLUS -> {
+                // !! nullが入るかのうせいがあるものにnull来ないよと証明する 強制アンラップ
+               val answer = text2.get()!!.toInt() + numberText.toInt()
+                text2.set(answer.toString())
+            }
+            SymbolTypeEnum.MINUS -> {
+                // !! nullが入るかのうせいがあるものにnull来ないよと証明する 強制アンラップ
+                val answer = text2.get()!!.toInt() - numberText.toInt()
+                text2.set(answer.toString())
+            }else ->{
+            text2.set("0")
+        }
+        }
+
+//        ↓のようにsetの中にwhenを含むこともできる
+//        text2.set(when(symbol.get()){
+//            SymbolTypeEnum.PLUS -> {
+//                // !! nullが入るかのうせいがあるものにnull来ないよと証明する 強制アンラップ
+//                val answer = text2.get()!!.toInt() + numberText.toInt()
+//
+//            }
+//            SymbolTypeEnum.MINUS -> {
+//                // !! nullが入るかのうせいがあるものにnull来ないよと証明する 強制アンラップ
+//                val answer = text2.get()!!.toInt() - numberText.toInt()
+//
+//            }else ->{
+//                0
+//            }
+//        }.toString())
+
+
+    }
+
+    enum class SymbolTypeEnum(val code: String, val label: String, val symbol: String){
+        PLUS("0", "足し算", "+"),
+        MINUS("1", "引き算", "-")
     }
 }
